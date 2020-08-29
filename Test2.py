4eff47d87c97725c7be71e30e43d6d817be45e58
diegojeda <66706753+diegojeda@users.noreply.github.com>
2020-08-29 12:33:12 -0500

    Deploy Heroku

--- Test2.py
+++ Test2.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import altair as alt
-import pydeck as pdk
+#import altair as alt
+#import pydeck as pdk
 import matplotlib.pyplot as plt
 
 DATE_TIME = "date/time"
